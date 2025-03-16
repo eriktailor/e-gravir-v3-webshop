4ad034f5181b45d65ebcbbebe66c74c58c7cdd8a
Erik Tailor <erno22szabo@gmail.com>
2025-03-16 18:10:50 +0100

    product listing working on

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
@@ -33,6 +34,23 @@ class Product extends Model
         return Attribute::make(
             get: fn ($value) => $value ? explode(',', $value) : [],
             set: fn ($value) => is_array($value) ? implode(',', $value) : $value,
+        );
+    }
+
+    /**
+     * Get the first image of the product
+     */
+    public function firstImageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                $image = $this->images->first();
+                if ($image && Storage::disk('public')->exists($image->image_path)) {
+                    return asset('storage/' . $image->image_path);
+                } else {
+                    return asset('/img/noimage.webp');
+                }
+            }
         );
     }
 
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -17,8 +17,27 @@
 <div class="container">
     <div class="products-list flex flex-col gap-3">
         
-        
-        
+        @forelse($products as $product)
+            <div class="p-6 bg-white shadow-md rounded-lg flex items-center justify-between gap-3">
+                <div class="flex items-center space-x-4">
+
+                    <img 
+                        src="{{ $product->first_image_url }}" 
+                        alt="{{ $product->name }} termékkép"
+                        class="w-18 h-18 rounded-full object-cover object-center">
+
+                    <div>
+                        <h3 class="mb-1">{{ $product->name }}</h3>
+                    </div>
+                </div>
+                <div class="flex justify-end items-center gap-x-4">
+                    <span class="px-2 py-1 bg-gray-200 text-sm rounded-md whitespace-nowrap">valami</span>     
+                    <x-button.chip icon="chevron-right" href="{{ route('products.edit', $product->id) }}"/>
+                </div>
+            </div>
+        @empty
+            <p>Nincs termék.</p>
+        @endforelse
 
     </div>
 </div>
