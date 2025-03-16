150af963873b9041602483715898951007159c27
Erik Tailor <erno22szabo@gmail.com>
2025-03-16 07:54:45 +0100

    few category form fixes, but update still not show image preview

--- resources/views/admin/categories/form.blade.php
+++ resources/views/admin/categories/form.blade.php
@@ -16,7 +16,7 @@
         </div>
     @endif
 
-    <form action="{{ isset($category) ? route('categories.update', $product) : route('categories.store') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ isset($category) ? route('categories.update', $category) : route('categories.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         @if(isset($category))
             @method('PUT')
@@ -25,16 +25,19 @@
         <div class="p-8 bg-white shadow-md rounded-lg mx-auto max-w-xl">
             <div class="flex flex-col gap-4">
                 <div class="form-group">
-                    <x-form.input for="name" label="Név" type="text"/>
+                    <x-form.input for="name" label="Név" type="text" :value="$category->name ?? ''"/>
                 </div>
                 <div class="form-group">
-                    <x-form.input for="slug" label="Egyedi slug" type="text"/>
+                    <x-form.input for="slug" label="Egyedi slug" type="text" :value="$category->slug ?? ''"/>
                 </div>
                 <div class="form-group">
-                    <x-form.textarea for="description" label="Leírás" rows="4"></x-form.textarea>
+                    <x-form.textarea for="description" label="Leírás" rows="4">{{ old('description', $category->description ?? '') }}</x-form.textarea>
                 </div>
                 <div class="form-group">
                     <input type="file" id="image" name="image">
+                    @if(isset($category) && $category->image)
+                        <input type="hidden" id="existingImage" value="{{ asset('storage/' . $category->image) }}">
+                    @endif
                 </div>
                 <x-button type="submit" class="">
                     {{ isset($category) ? 'Frissítés' : 'Létrehozás' }}
