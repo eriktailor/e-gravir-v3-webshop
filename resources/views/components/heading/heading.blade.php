ccd7934f08fd370824a17367907ab131c7c222c3
Erik Tailor <erno22szabo@gmail.com>
2025-03-18 02:46:40 +0100

    save

--- resources/views/components/heading/heading.blade.php
+++ resources/views/components/heading/heading.blade.php
@@ -5,7 +5,7 @@
 
 @php
     $defaultClasses = [
-        'h1' => 'font-semibold text-stone-950 text-5xl leading-[56px] tracking-tighter',
+        'h1' => 'font-bold text-stone-950 text-4xl leading-[48px] tracking-tighter',
         'h2' => 'font-semibold text-stone-950 text-2xl leading-[32px] tracking-tight',
         'h3' => 'font-semibold text-stone-950 text-xl leading-[28px] tracking-tight',
         'h4' => 'font-semibold text-stone-950 text-base leading-[22px]',
