042f954e8f4f557820dfd68b5c29f94daa4da3ee
Erik Tailor <erno22szabo@gmail.com>
2025-03-20 22:06:14 +0100

    crt animation added

--- resources/js/app.js
+++ resources/js/app.js
@@ -33,7 +33,39 @@ $('.button-submit').on('click', function(e) {
 });
 
 
+/**
+ * Cart button clicked, open the sidebar cart
+ */
+$('#cartToggle').on('click', function() {
+    $('body').addClass('overflow-hidden');
 
+    // Make #sideCart visible
+    $('#sideCart').removeClass('invisible opacity-0').addClass('opacity-100');
+
+    // Animate backdrop fade-in
+    $('#cartBackdrop').removeClass('opacity-0').addClass('opacity-100');
+
+    // Animate cart panel slide-in
+    $('#cartPanel').removeClass('translate-x-full').addClass('translate-x-0');
+});
+
+/**
+ * Cart close button or outside clicked, hide the sidecart
+ */
+$('#cartBackdrop, .close-cart').on('click', function() {
+    $('body').removeClass('overflow-hidden');
+
+    // Fade out backdrop
+    $('#cartBackdrop').removeClass('opacity-100').addClass('opacity-0');
+
+    // Slide out panel
+    $('#cartPanel').removeClass('translate-x-0').addClass('translate-x-full');
+
+    // After animation, hide entire aside
+    setTimeout(() => {
+        $('#sideCart').removeClass('opacity-100').addClass('opacity-0 invisible');
+    }, 300); // Match duration!
+});
 
 
 });
--- resources/views/components/navbar/cart.blade.php
+++ resources/views/components/navbar/cart.blade.php
@@ -1,10 +1,14 @@
-<aside class="nav-cart w-full h-screen fixed top-0 right-0 z-50" id="navCart">
-    <div class="bg-stone-950/50 absolute left-0 top-0 w-full h-full z-50"></div>
-    <div class="absolute p-8 right-0 top-0 z-50 h-full">
-        <div class="bg-white h-full w-full sm:w-[500px] rounded-xl">
-            <div class="cart-header p-8 flex justify-between">
+<aside id="sideCart" class="nav-cart fixed inset-0 z-50 invisible opacity-0 transition-opacity duration-300">
+
+    <!-- BACKDROP -->
+    <div id="cartBackdrop" class="absolute inset-0 bg-stone-950/50 opacity-0 transition-opacity duration-300"></div>
+
+    <!-- CART PANEL -->
+    <div id="cartPanel" class="absolute right-0 top-0 h-full w-full sm:w-[500px] transform translate-x-full transition-transform duration-300 ease-in-out p-8">
+        <div class="bg-white rounded-xl h-full p-8">
+            <div class="cart-header flex justify-between">
                 <x-heading level="h2">Kosaram</x-heading>
-                <x-button.chip icon="x"/>
+                <x-button.chip icon="x" class="close-cart"/>
             </div>
             <div class="cart-content h-[calc(100%-100px)] flex justify-center">
                 @forelse(session('cart', []) as $id => $item)
@@ -14,16 +18,14 @@
                     </div>
                 @empty
                     <div class="flex flex-col items-center gap-y-6">
-                        <img class="px-8 -mt-8" src="{{ asset('/img/empty_cart.png') }}" alt="Üres kosár">
+                        <img class="px-8" src="{{ asset('/img/empty_cart.png') }}" alt="Üres kosár">
                         <p>Jelenleg nincs termék a kosaradban.</p>
                         <x-button href="{{ route('webshop.home') }}">Vásárlás Folytatása</x-button>
                     </div>
                 @endforelse
             </div>
-            <div class="cart-footer">
-
-            </div>
+            <div class="cart-footer"></div>
         </div>
     </div>
-    
+
 </aside>
