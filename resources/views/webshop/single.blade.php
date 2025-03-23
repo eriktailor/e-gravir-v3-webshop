eda0038b3eaed0489bc7e4b9f6d38e9ec5ebc488
Erik Tailor <erno22szabo@gmail.com>
2025-03-23 16:02:48 +0100

    single product page updated

--- resources/views/webshop/single.blade.php
+++ resources/views/webshop/single.blade.php
@@ -1,1 +1,15 @@
-single
+@extends('layouts.shop')
+
+@section('title', $product->name)
+
+@section('content')
+
+    <x-header.page :title="$product->name"/>
+
+    <main>
+        <div class="container">
+            
+        </div>
+    </main>
+
+@endsection
