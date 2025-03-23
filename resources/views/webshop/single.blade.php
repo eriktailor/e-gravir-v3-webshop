02c8f13903c9c9819c8a5bf4b1b5b4752c6122a1
Erik Tailor <erno22szabo@gmail.com>
2025-03-23 23:29:07 +0100

    single page update

--- resources/views/webshop/single.blade.php
+++ resources/views/webshop/single.blade.php
@@ -11,11 +11,20 @@
             <div class="grid grid-cols-2">
 
                 <!-- Product Gallery-->
-                <div class="flex">
+                <div class="flex gap-x-2">
                     <div class="flex flex-col gap-2">
                         @foreach($images as $image)
-                            <img src="/storage/{{ $image->image_path }}" alt="{{ $product->name }} termék kép">
+                            <img src="/storage/{{ $image->image_path }}" 
+                                alt="{{ $product->name }} termék kép"
+                                class="w-[50px] h-[50px] object-cover object-center flex-none rounded">
                         @endforeach
+                    </div>
+                    <div class="flex grow">
+                        @if(isset($images[0]))
+                            <img src="/storage/{{ $images[0]->image_path }}" 
+                                alt="{{ $product->name }}" 
+                                class="w-full h-full object-cover object-center rounded-lg">
+                        @endif
                     </div>
                 </div>
 
