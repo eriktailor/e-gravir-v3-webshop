0471b347afbe3c6f0a519d5b87b7a3d09683ffaa
Erik Tailor <erno22szabo@gmail.com>
2025-03-24 16:44:18 +0100

    form styling added

--- resources/views/components/form/error.blade.php
+++ resources/views/components/form/error.blade.php
@@ -1,5 +1,5 @@
 @props(['for'])
 
 @error($for)
-    <span id="{{ $for }}-error" class="text-red-500 text-sm font-medium">{{ $message }}</span>
+    <span id="{{ $for }}-error" class="error-message text-red-500 text-sm font-medium">{{ $message }}</span>
 @enderror
