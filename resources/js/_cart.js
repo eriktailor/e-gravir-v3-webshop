e5143db1d9a27b788554e441ce223ceac21c219b
Erik Tailor <erno22szabo@gmail.com>
2025-03-27 17:54:29 +0100

    archive page, product added to cart dynamically seems to work now

--- resources/js/_cart.js
+++ resources/js/_cart.js
@@ -2,67 +2,37 @@
  * COMPONENT: Cart
  * -----------------------------------------------------------------------------------
  */
+import initDropdown from './_dropdown';
 
 export default function initCart() {
 
     /**
-     * Open cart sidebar function
+     * Item added to cart
      */
-    function openCartSidebar() {
-        const $sideCart = $('#sideCart');
+    $('.add-to-cart-btn').on('click', function(e) {
+        e.preventDefault();
 
-        $('body').addClass('overflow-hidden');
-        $sideCart.removeClass('invisible opacity-0').addClass('opacity-100');
-        $sideCart.find('.offcanvas-backdrop').removeClass('opacity-0').addClass('opacity-100');
-        $sideCart.find('.offcanvas-panel').removeClass('translate-x-full').addClass('translate-x-0');
-    }
+        
 
-    /**
-     * Reload cart partials function
-     */
-    function reloadCartContent(count = null) {
-        $('#sideCart .cart-header').load(location.href + ' #sideCart .cart-header > *');
-        $('#sideCart .cart-content').load(location.href + ' #sideCart .cart-content > *');
-        $('#sideCart .cart-footer').load(location.href + ' #sideCart .cart-footer > *', function() {
-            // If count not passed (initial load), read from data attribute
-            let itemCount = count !== null 
-                ? count 
-                : parseInt($('#sideCart').data('cart-count'));
-    
-            if (itemCount === 0) {
-                $('#sideCart .cart-footer').addClass('hidden');
-            } else {
-                $('#sideCart .cart-footer').removeClass('hidden');
-            }
-        });
-    }
+        const productId = $(this).data('id');
+        const btn = $(this);
+        btn.prop('disabled', true).text('Hozzáadás...');
 
-    /**
-     * Add to cart button clicked
-     */
-    $('.add-to-cart-btn').on('click', function() {
-        let productId = $(this).data('id');
-        $.post('/webshop/cart/add/' + productId, function(response) {
-            $('.cart-count').text(response.count);
-            reloadCartContent(response.count); // pass count
-            openCartSidebar();
-        });
+        $.post("/webshop/cart/add/" + productId, { quantity: 1 })
+            .done(function(response) {
+                $('#miniCart').load(location.href + ' #miniCart > *', function() {
+                    initDropdown(); // újraindítjuk a dropdown eventeket
+                    $('#miniCart .dropdown-toggle').trigger('click'); // nyitjuk a cartot
+                });
+            })
+            .fail(function() {
+                alert('Hiba történt a kosárhoz adás során.');
+            })
+            .always(function() {
+                btn.prop('disabled', false).text('Kosárba teszem');
+            });
+        
+        
     });
-
-    /**
-     * Remove from cart button clicked
-     */
-    $(document).on('click', '.remove-cart-item', function() {
-        let productId = $(this).data('id');
-        $.post('/webshop/cart/remove/' + productId, function(response) {
-            $('.cart-count').text(response.count);
-            reloadCartContent(response.count); // pass count
-        });
-    });
-
-    /**
-     * Initially reload cart partials (passing null, no check needed here)
-     */
-    reloadCartContent();
 
 }
